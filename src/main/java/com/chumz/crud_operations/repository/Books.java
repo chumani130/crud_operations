abf797cd64e231833ba1af7de7742eee62ed8a17
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-13 13:09:06 +0200

    Delete Books.java

--- src/main/java/com/chumz/crud_operations/repository/Books.java
+++
@@ -1,8 +0,0 @@
-package com.chumz.crud_operations.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Books extends JpaRepository<Books, Integer> {
-}
