6ae39143d9839c3657510dd02017f18eff579f17
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-13 13:09:18 +0200

    Update BooksService.java

--- src/main/java/com/chumz/crud_operations/services/BooksService.java
+++ src/main/java/com/chumz/crud_operations/services/BooksService.java
@@ -1,4 +1,21 @@
 package com.chumz.crud_operations.services;
 
+import com.chumz.crud_operations.dto.BookRequest;
+import com.chumz.crud_operations.dto.BookResponse;
+
+import java.util.List;
+
 public interface BooksService {
+
+    // save
+    BookResponse saveBook(BookRequest bookRequest);
+
+    // read
+    List<BookResponse> fetchBookList();
+
+    // update
+    BookResponse updateBook(BookRequest bookRequest, Integer id);
+
+    // delete
+    void deleteBookById(Integer id);
 }
