4f7017fa6d974dd7b40003364f5f9bc0a9e417d3
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-13 13:08:41 +0200

    Update BookRequest.java

--- src/main/java/com/chumz/crud_operations/dto/BookRequest.java
+++ src/main/java/com/chumz/crud_operations/dto/BookRequest.java
@@ -1,5 +1,14 @@
 package com.chumz.crud_operations.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookRequest {
 
     private String name;
