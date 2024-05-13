2951c5334f7943f96e4efb0a3f4623b7cf1c95df
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-05-13 11:37:00 +0200

    Update Books.java

--- src/main/java/com/chumz/crud_operations/repository/Books.java
+++ src/main/java/com/chumz/crud_operations/repository/Books.java
@@ -1,4 +1,8 @@
 package com.chumz.crud_operations.repository;
 
-public class Books {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Books extends JpaRepository<Books, Integer> {
 }
